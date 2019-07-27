146e23763930e21c7d4b2339aed81361dc860ebb
Devan Farrell <devan.farrell16@gmail.com>
2019-07-26 17:08:26 -0700

    input updates

--- client/src/views/circuit/inputs.js
+++ client/src/views/circuit/inputs.js
@@ -5,12 +5,21 @@ import { selectInputs } from 'redux/circuit/selectors';
 import Toggle from 'components/toggle';
 import colors from 'styles/colors';
 
+const TypedInput = props => {
+	return (
+		<InputWrapper>
+			<StyledInput placeholder="0000" />
+			<BaseSelection>DEC</BaseSelection>
+		</InputWrapper>
+	);
+};
+
 const MultipleInput = ({ label, id }) => {
 	return (
-		<SingleInputWrapper>
-			<span>{label}</span>
-			<Toggle id={id} />
-		</SingleInputWrapper>
+		<MultipleInputWrapper>
+			<div>{label}</div>
+			<TypedInput id={id} />
+		</MultipleInputWrapper>
 	);
 };
 
@@ -38,6 +47,38 @@ const Inputs = props => {
 		</>
 	);
 };
+
+const BaseSelection = styled.div`
+	border-top-right-radius: 3px;
+	border-bottom-right-radius: 3px;
+	border-left: solid 1px hotpink;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 40px;
+`;
+
+const StyledInput = styled.input`
+    border-width: 0px;
+    flex: 1 0 auto;
+    height: 100%;
+`;
+
+const InputWrapper = styled.div`
+	background-color: white;
+	padding: 5px;
+    border-radius: 3px;
+    display: flex;
+	color: ${colors.grey.veryDark};
+`;
+
+const MultipleInputWrapper = styled.div`
+	color: ${colors.grey.extraLight};
+	margin: 0 5px;
+	padding: 5px 0;
+	align-items: center;
+`;
 
 const SingleInputWrapper = styled.div`
 	color: ${colors.grey.extraLight};
