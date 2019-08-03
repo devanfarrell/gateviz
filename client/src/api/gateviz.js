e734eb54d04aede7e526f12c4881edfe5f404273
Devan Farrell <devan.farrell16@gmail.com>
2019-08-03 09:47:20 -0700

    New server structure

--- client/src/api/gateviz.js
+++ client/src/api/gateviz.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const fetchCircuitList = async () => {
-	return await axios.get('/v1/circuits');
+	return await axios.get('/v1/circuit_list');
 };
 
 export const fetchCircuitRequest = async cid => await axios.get(`/v1/circuit/${cid}`);
---
+++ server/data/circuitList.js
@@ -0,0 +1,26 @@
+const circuitList = () => ({
+	list: [
+		{
+			name: 'Thermometer',
+			cid: 'vq8ev7ga',
+			description: 'A simple three input thermometer circuit'
+		},
+		{
+			name: 'Thermometer abstracted',
+			cid: 'vq8ev7ga2',
+			description: 'A simple three input thermometer circuit'
+		},
+		{
+			name: 'Thermometer abstracted bus',
+			cid: 'vq8ev7ga3',
+			description: 'A simple three input thermometer circuit with a bus'
+		},
+		{
+			name: 'Gates Demo',
+			cid: 'asdfasdfasdf',
+			description: 'An example of gate functionality'
+		}
+	]
+});
+
+export default circuitList;
