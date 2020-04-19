d470e0c4775dc3b6f5a46c7528c49d85174bc575
Devan Farrell <devan.farrell16@gmail.com>
2020-04-19 13:46:28 -0700

    Removed unused code

--- client/src/redux/slices/circuit/index.ts
+++
@@ -1,5 +0,0 @@
-import { createSlice } from "redux-dogma";
-import { stateSlice } from "./state";
-import { dependenciesSlice } from "./dependencies";
-
-export const circuitSlice = createSlice("Circuit").addSlice(stateSlice).addSlice(dependenciesSlice);
--- client/src/redux/slices/circuit/state.ts
+++
@@ -1,8 +0,0 @@
-import { createSlice } from "redux-dogma";
-
-export const stateSlice = createSlice("State", {});
-
-export const changeInputs = stateSlice.createAction<{ id: string; value: boolean; circuit: any }>(
-  "CHANGE_INPUTS",
-  (draft, payload) => {}
-);
