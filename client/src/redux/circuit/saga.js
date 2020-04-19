5a554112dc278f65969b25c015f2307a0d4d6dbc
Devan Farrell <devan.farrell16@gmail.com>
2020-04-19 13:51:23 -0700

    Remove unneeded dependancy

--- client/src/redux/circuit/saga.js
+++ client/src/redux/circuit/saga.js
@@ -1,41 +1,38 @@
-import { takeLatest, put, select, takeEvery } from 'redux-saga/effects';
-import {
-	FETCH_CIRCUIT_REQUEST,
-	fetchCircuitSuccess,
-	CHANGE_INPUTS_REQUEST,
-	changeInputsSuccess
-} from './actions';
-import { fetchCircuitRequest } from 'api/gateviz';
-import processor from './processor';
-import { selectAutoEvaluate } from './selectors';
-import evaluateCircuit from './evaluateCircuit';
+import { effects } from "redux-dogma";
+import { FETCH_CIRCUIT_REQUEST, fetchCircuitSuccess, CHANGE_INPUTS_REQUEST, changeInputsSuccess } from "./actions";
+import { fetchCircuitRequest } from "api/gateviz";
+import processor from "./processor";
+import { selectAutoEvaluate } from "./selectors";
+import evaluateCircuit from "./evaluateCircuit";
+
+const { takeLatest, put, select, takeEvery } = effects;
 
 function* fetchCircuitHandler(action) {
-	const { data } = yield fetchCircuitRequest(action.payload);
-	const processedCircuit = yield processor(data);
-	evaluateCircuit(processedCircuit);
-	yield put(fetchCircuitSuccess({ response: data, parsedCircuit: processedCircuit }));
+  const { data } = yield fetchCircuitRequest(action.payload);
+  const processedCircuit = yield processor(data);
+  evaluateCircuit(processedCircuit);
+  yield put(fetchCircuitSuccess({ response: data, parsedCircuit: processedCircuit }));
 }
 
 function* changeInputsHandler(action) {
-	const { id, value, circuit } = action.payload;
-	const index = circuit.input.findIndex(input => input.id === id);
-	circuit.input[index].state = value;
-	const autoEvaluate = yield select(selectAutoEvaluate);
-	if (autoEvaluate) {
-		evaluateCircuit(circuit);
-	}
-	yield put(
-		changeInputsSuccess({
-			input: circuit.input,
-			parts: circuit.parts,
-			output: circuit.output,
-			name: circuit.name
-		})
-	);
+  const { id, value, circuit } = action.payload;
+  const index = circuit.input.findIndex((input) => input.id === id);
+  circuit.input[index].state = value;
+  const autoEvaluate = yield select(selectAutoEvaluate);
+  if (autoEvaluate) {
+    evaluateCircuit(circuit);
+  }
+  yield put(
+    changeInputsSuccess({
+      input: circuit.input,
+      parts: circuit.parts,
+      output: circuit.output,
+      name: circuit.name,
+    })
+  );
 }
 
 export default function* saga() {
-	yield takeLatest(FETCH_CIRCUIT_REQUEST, fetchCircuitHandler);
-	yield takeEvery(CHANGE_INPUTS_REQUEST, changeInputsHandler);
+  yield takeLatest(FETCH_CIRCUIT_REQUEST, fetchCircuitHandler);
+  yield takeEvery(CHANGE_INPUTS_REQUEST, changeInputsHandler);
 }
